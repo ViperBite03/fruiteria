da968974df0b853ecfc42057f27445d990da61c0
ViperBite03 <layitamartin@gmail.com>
2025-05-18 21:50:11 +0200

    fotos 2.0

--- src/components/Portada.vue
+++ src/components/Portada.vue
@@ -5,7 +5,7 @@
     height: 100vh;
     background-color: #f9f8ee;
 
-    background-image: url('public/dark-bg.png');
+    background-image: url('dark-bg.png');
 
     display: flex;
     align-items: center;
